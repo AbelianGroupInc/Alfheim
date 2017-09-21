5a95b577a3a1597ab47b76302a1d0af3e505efeb
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2017-09-21 14:54:57 +0300

    Добавлен стартовый экран.

--- Alfheim.GUI/App.xaml.cs
+++ Alfheim.GUI/App.xaml.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace Alfheim.GUI
@@ -13,5 +12,79 @@ namespace Alfheim.GUI
     /// </summary>
     public partial class App : Application
     {
+        private static List<CultureInfo> m_Languages = new List<CultureInfo>();
+
+        public static event EventHandler LanguageChanged;
+        public static List<CultureInfo> Languages
+        {
+            get
+            {
+                return m_Languages;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+            App.LanguageChanged += App_LanguageChanged;
+
+            m_Languages.Clear();
+
+            m_Languages.Add(new CultureInfo("en-US"));
+            m_Languages.Add(new CultureInfo("ru-RU"));
+
+            Language = Alfheim.GUI.Properties.Settings.Default.DefaultLanguage;
+        }
+
+        public static CultureInfo Language
+        {
+            get
+            {
+                return System.Threading.Thread.CurrentThread.CurrentUICulture;
+            }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("value");
+                if (value == System.Threading.Thread.CurrentThread.CurrentUICulture)
+                    return;
+
+                System.Threading.Thread.CurrentThread.CurrentUICulture = value;
+
+                ResourceDictionary dict = new ResourceDictionary();
+
+                switch (value.Name)
+                {
+                    case "ru-RU":
+                        dict.Source = new Uri(String.Format("Resources/lang.{0}.xaml", value.Name), UriKind.Relative);
+                        break;
+                    default:
+                        dict.Source = new Uri("Resources/lang.xaml", UriKind.Relative);
+                        break;
+                }
+
+                ResourceDictionary oldDict = (from d in Application.Current.Resources.MergedDictionaries
+                                              where d.Source != null && d.Source.OriginalString.StartsWith("Resources/lang.")
+                                              select d).First();
+                if (oldDict != null)
+                {
+                    int ind = Application.Current.Resources.MergedDictionaries.IndexOf(oldDict);
+                    Application.Current.Resources.MergedDictionaries.Remove(oldDict);
+                    Application.Current.Resources.MergedDictionaries.Insert(ind, dict);
+                }
+                else
+                {
+                    Application.Current.Resources.MergedDictionaries.Add(dict);
+                }
+
+                LanguageChanged(Application.Current, new EventArgs());
+            }
+        }
+
+        private void App_LanguageChanged(Object sender, EventArgs e)
+        {
+            Alfheim.GUI.Properties.Settings.Default.DefaultLanguage = Language;
+            Alfheim.GUI.Properties.Settings.Default.Save();
+        }
     }
 }
