b6cd6e8875db925ebda161f43141eafc55ba7102
fofancy <shaylin3@gmail.com>
2017-09-25 21:41:35 +0300

    Fixed bug in Term.SetFunction()

---
+++ Alfheim.FuzzyLogic/Variables/Model/LinguisticVariableIsNotSpecifiedException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alfheim.FuzzyLogic.Variables.Model
+{
+    public class LinguisticVariableIsNotSpecifiedException : FuzzyLogicException
+    {
+        public LinguisticVariableIsNotSpecifiedException(string message) : base(message)
+        {
+        }
+
+        public LinguisticVariableIsNotSpecifiedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected LinguisticVariableIsNotSpecifiedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
